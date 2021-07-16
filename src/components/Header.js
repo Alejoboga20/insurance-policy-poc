413b56b824370e403f3b83682fa943359678062a
Alejo Boga <alejoboga20@gmail.com>
2021-07-16 11:30:33 -0300

    Adding styled components in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
+import styled from '@emotion/styled';
 import React from 'react';
+
+const HeaderContainer = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', serif;
+  text-align: center;
+`;
 
 export const Header = ({ title }) => {
   return (
-    <header>
-      <h1>{title}</h1>
-    </header>
+    <HeaderContainer>
+      <HeaderText>{title}</HeaderText>
+    </HeaderContainer>
   );
 };
