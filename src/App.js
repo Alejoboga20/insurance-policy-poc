76033a3ed7b460058e0d417d82b1931191491256
Alejo Boga <alejoboga20@gmail.com>
2021-07-20 10:58:04 -0300

    Creating containers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import { Header } from './components/Header';
 
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
 export const App = () => {
   return (
-    <div>
+    <Container>
       <Header title='Insurance Policy' />
-    </div>
+      <FormContainer></FormContainer>
+    </Container>
   );
 };
